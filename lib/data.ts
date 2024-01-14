72d2660ee5d4ebbfc7cf17b629248cc0954d1921
vikraamravi@gmail.com <vikraamravi@gmail.com>
2024-01-13 23:18:52 -0500

    fixed path details

--- lib/data.ts
+++ lib/data.ts
@@ -1,8 +1,7 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import escapeAtlasImg from "@/public/escapeAtlas.jpg";
+import escapeAtlasImg from "@/public/EscapeAtlas.jpg";
  
 
 export const links = [
